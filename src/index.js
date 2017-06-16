179ea97f3f5a99878f4ebfe32778ce00cc8d1cbd
Magnus Holm <mail@magnusholm.com>
2017-06-16 15:30:09 +0100

    add api call til youtube in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+
+
+const API_KEY = 'AIzaSyBCxDRZbOaEUTO7Qbewh6TDfTrkZw85t-0';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 // Create a new component. This component should produce som HTML.
 const App = () => {
-  return <div>hi</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 
