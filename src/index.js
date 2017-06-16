308c7258bc510eb0a3c3fe4635b22fab86d89b13
Magnus Holm <mail@magnusholm.com>
2017-06-16 15:56:23 +0100

    pass videos to video_list component via props

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyBCxDRZbOaEUTO7Qbewh6TDfTrkZw85t-0';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-// Create a new component. This component should produce som HTML.
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos: videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-
-// Take this component's generated html and put it on the page. (In the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
