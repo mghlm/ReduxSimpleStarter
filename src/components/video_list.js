41ee949a641e1f8622912c2e50f8c96bca1f5d23
Magnus Holm <mail@magnusholm.com>
2017-06-16 16:32:05 +0100

    VideoList component creates new array of VideoListItem components and pass each video element to each of them as props

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import VideoListItem from './component/video_list_item';
+import VideoListItem from './video_list_item';
 
-const VideoListItem = (props) => {
+const VideoList = (props) => {
+
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem key={video.etag} video={video} />
+  });
+
   return (
     <ul className="col-md-4 list-group">
-
+      {videoItems}
     </ul>
   );
 };
 
-export default VideoListItem
+export default VideoList
