741714b105d320f853f381ffe221d8a193f31448
Magnus Holm <mail@magnusholm.com>
2017-06-16 16:07:56 +0100

    Add VideoListItem component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import VideoListItem from './component/video_list_item';
 
-const VideoList = (props) => {
+const VideoListItem = (props) => {
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+
     </ul>
   );
 };
 
-export default VideoList
+export default VideoListItem
