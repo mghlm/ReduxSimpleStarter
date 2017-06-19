6462c7bd9014c07327d7b5b34c6b471862dc7738
Magnus Holm <mail@magnusholm.com>
2017-06-19 14:38:09 +0100

    send new api call every time input term changes and update seach results

--- src/index.js
+++ src/index.js
@@ -15,6 +15,10 @@ class App extends Component {
       selectedVideo: null
     };
 
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
       this.setState({
         videos: videos ,
@@ -26,7 +30,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
