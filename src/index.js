e1e9997d3edce63cf1ae4fa8c9d6a909287256dd
Magnus Holm <mail@magnusholm.com>
2017-06-19 13:24:36 +0100

    onSelectedVideo gets passed from index.js to video_list to video_list_item and updated when video gets clicked, then rerenders with that video

--- src/index.js
+++ src/index.js
@@ -16,7 +16,10 @@ class App extends Component {
     };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos: videos });
+      this.setState({
+        videos: videos ,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -24,8 +27,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
       </div>
     );
   }
